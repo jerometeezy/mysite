a9b77ac842cfeff397518d020480bdaa39a470cd
Jerome Nosa <jtz05@24dcaea7d349>
2018-05-04 15:32:07 +0000

    Added database configuration to the flask app.

---
+++ config.py
@@ -0,0 +1,7 @@
+class BaseConfig(object):
+    SQLALCHEMY_DATABASE_URI = (
+        'mysql://<jtz05>$mysitedb:'
+        '<database jtz12345>'
+        '@<jtz05>.mysql.pythonanywhere-services.com/'
+        '<jtz05>$mysitedb')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- flask_app.py
+++ flask_app.py
@@ -4,10 +4,14 @@
 from flask import Flask
 from flask import render_template
 import constants
+from flask_sqlalchemy import SQLAlchemy
+
+
 
 
 app = Flask(__name__)
-
+app.config.from_object('config.BaseConfig')
+db = SQLAlchemy(app)
 
 
 @app.route('/about_me')
